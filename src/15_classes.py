953d74533235aaee286bb6191f748860ea24fe2f
Scott Renz <scottrenz@scottrenz.com>
2019-12-03 16:04:11 -0500

    prettied up 15

--- src/15_classes.py
+++ src/15_classes.py
@@ -6,11 +6,11 @@ class LatLon:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
-    def c(self):
-        return {"lat": self.lat, "lon": self.lon}
-x = LatLon.c(LatLon(1,2))
+    def __str__(self):
+        x={"lat": self.lat, "lon": self.lon}
+        return f'\n\tLatLon\n\n{x}'
+x = LatLon.__str__(LatLon(111.23,-42.333))
 print(x)        
-geocache = 2        
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 class Waypoint(LatLon):
@@ -18,7 +18,7 @@ class Waypoint(LatLon):
         super().__init__(lat,lon)
         self.name = name
     def c(self):
-        return {"name": self.name, "lat": self.lat, "lon": self.lon}
+        return f'\n\tWaypoint\nName:\t\t{self.name}\nLatitude:\t{self.lat}\nLongitude:\t{self.lon}\n'
 waypoint = Waypoint.c(Waypoint("Way",1,2))
 # YOUR CODE HERE
 
@@ -31,9 +31,9 @@ class Geocache(Waypoint):
         super().__init__(name, lat, lon)
         self.difficulty = difficulty
         self.size = size
-    def c(self):
-        return {"name": self.name, "difficulty": self.difficulty, "size": self.size, "lat": self.lat, "lon": self.lon}
-geocache = Geocache.c(Geocache("Way","hard","large",1,2))
+    def __str__(self):
+        return f'\n\tGeocache\nName:\t\t{self.name}\nDifficulty:\t{self.difficulty}\nSize:\t\t{self.size}\nLatitude:\t{self.lat}\nLongitude:\t{self.lon}\n'
+geocache = Geocache.__str__(Geocache("Way","hard","large",1,2))
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 waypoint = Waypoint.c(Waypoint("Catacombs", 41.70505, -121.51521))
@@ -47,7 +47,7 @@ print(waypoint)
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
-geocache = Geocache.c(Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556))
+geocache = Geocache.__str__(Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556))
 
 # Print it--also make this print more nicely
 print(geocache)
