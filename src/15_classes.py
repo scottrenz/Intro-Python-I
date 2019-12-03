1e9c980deeda70612412cd959ea1ae77d0323623
Scott Renz <scottrenz@scottrenz.com>
2019-12-02 19:07:26 -0500

    up to 14 started on 15

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,10 +2,26 @@
 # constructor
 
 # YOUR CODE HERE
-        
+class MyParentClass():
+    def __init__(self, x, y):
+        return x
+class SubClass(MyParentClass):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+class LatLon():
+    def c(self,lat,lon):
+        return {'lat': lat,'lon': lon}
+x = LatLon        
+x = x.c(x,1,2)
+print(x)
+waypoint = 1
+geocache = 2        
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
-
+class Waypoint(LatLon):
+    super().__init__(name)
+    x = LatLon.name=name
+    return x
 # YOUR CODE HERE
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
