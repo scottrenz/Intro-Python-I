ec2ba83f649823c8298688b3ed6a965a81cb37a9
Scott Renz <scottrenz@scottrenz.com>
2019-12-05 03:30:01 -0500

    made prime more efficient

--- src/prime.py
+++ src/prime.py
@@ -1,15 +1,26 @@
+import math
 num = input("Enter a number: ")
 num = abs(int(num))
 r=0
 v=0
-for v in range(2,num):
+n = int(math.sqrt(num))
+if num < 2:
+    print('%d is not prime' % num)
+    exit()
+if num == 2 or num == 3 or num == 5 or num == 7 or num == 11 or num == 13:
+    print('%d is prime' % num)
+    exit()
+if num % 2 == 0:
+    print('%d is not prime' % num)
+    exit()
+for v in range(3,n,2):
     r = num % v
     if r == 0:
         break
-print(num,r,v)
+print(' number %d' % num,' remainder %d' % r,'divisor %d' % v, 'square root %d' % n)
 
-if v == num -1 or (r == 0 and v == 0):
-    print(f'{num} is prime')
+if n - v <= 2:
+    print('%d is prime' % num)
 else:
-    print(f'{num} is not prime')
+    print('%d is not prime' % num)
     
